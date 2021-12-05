2e6faf7cd7b0f48ca5b65c6016dbc92c43c6e9de
Adewole1 <madedayo@gmail.com>
2021-12-05 14:44:51 +0100

    update chat room

--- educa/settings.py
+++ educa/settings.py
@@ -22,7 +22,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['crashcourse.com', '127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1']
 
 # Application definition
 
@@ -148,7 +148,7 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            'hosts': [os.environ.get('REDIS_URL', 'redis://localhost:6379')],
+            'hosts': [('127.0.0.1', 6379)],
         },
     },
 }
@@ -169,3 +169,12 @@ if os.getcwd() == '/app':
     dotenv_path = os.path.join(BASE_DIR, '.env')
     load_dotenv(dotenv_path)
     SECRET_KEY = os.environ.get('SECRET_KEY')
+    
+    CHANNEL_LAYERS = {
+        'default': {
+            'BACKEND': 'channels_redis.core.RedisChannelLayer',
+            'CONFIG': {
+                'hosts': [os.environ.get('REDIS_URL', 'redis://localhost:6379')],
+            },
+        },
+    }
