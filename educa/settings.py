b7cb95e40839b38a17065b0384aa5ef685e0ed2e
Adewole1 <madedayo@gmail.com>
2021-12-10 01:05:37 +0100

    Added error templates

--- educa/settings.py
+++ educa/settings.py
@@ -61,7 +61,7 @@ ROOT_URLCONF = 'educa.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'educa/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
